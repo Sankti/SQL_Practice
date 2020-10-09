7856461a921cc300ffa8abb85e7f1d999ad822c2
Adam <57396704+Sankti@users.noreply.github.com>
2020-10-09 18:45:53 +0200

    Create zapytania.sql

---
+++ zapytania.sql
@@ -0,0 +1,34 @@
+-- 1. Wyświetl całość informacji z relacji TEAMS.
+SELECT * FROM TEAMS;
+
+-- 2. Wyświetl całość informacji z relacji użytkownicy.
+SELECT * FROM TEAMS;
+
+-- 3. Wyświetl nazwiska i roczne dochody pracowników.
+SELECT
+	LASTNAME,
+	SALARY_BASE * 12
+FROM USERS;
+
+-- 4. Wyświetl nazwy etatów i sumaryczne miesięczne dochody pracowników.
+
+
+-- 5. Wyświetl całość informacji o zespołach sortując wynik według nazw zespołów
+
+
+-- 6. Wyświetl listę stanowisk (bez duplikatów) na których zatrudnieni są pracownicy.
+
+
+-- 7. Wyświetl wszystkie informacje o juniorach pracujących w firmie.
+
+
+-- 8. Posortuj dane pracowników po dwóch kolumnach, najpierw alfabetycznie wg nazwisk i malejąco wg pensji.
+
+
+-- 9. Sprawdź jak działają metody UPPER/LOWER/REPLACE dla MySQL. Wyświetl nazwy stanowisk małymi literami w kolejności alfabetycznej.
+
+
+-- 10. Znajdź wszystkich pracowników, których bonus roczny będzie większy niż dwukrotność pensji miesięcznej
+
+
+-- 11. Wyszukaj wszystkich pracowników, których id jest większe niż 150
