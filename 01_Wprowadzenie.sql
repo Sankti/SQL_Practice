f85f1347e14b43e8b17af5e9efda93a4ea377490
Adam <57396704+Sankti@users.noreply.github.com>
2020-10-10 14:50:05 +0200

    updating

--- 01_Wprowadzenie.sql
+++ 01_Wprowadzenie.sql
@@ -11,24 +11,36 @@ SELECT
 FROM USERS;
 
 -- 4. Wyświetl nazwy etatów i sumaryczne miesięczne dochody pracowników.
-
+SELECT
+	POSITION,
+    SALARY_BASE + SALARY_ADD
+FROM USERS;
 
 -- 5. Wyświetl całość informacji o zespołach sortując wynik według nazw zespołów
-
+SELECT * FROM TEAMS
+ORDER BY NAME;
 
 -- 6. Wyświetl listę stanowisk (bez duplikatów) na których zatrudnieni są pracownicy.
-
+SELECT
+	DISTINCT POSITION
+FROM USERS;
 
 -- 7. Wyświetl wszystkie informacje o juniorach pracujących w firmie.
-
+SELECT * FROM USERS
+WHERE POSITION = 'JUNIOR';
 
 -- 8. Posortuj dane pracowników po dwóch kolumnach, najpierw alfabetycznie wg nazwisk i malejąco wg pensji.
-
+SELECT * FROM USERS
+ORDER BY LASTNAME ASC, SALARY_BASE DESC;
 
 -- 9. Sprawdź jak działają metody UPPER/LOWER/REPLACE dla MySQL. Wyświetl nazwy stanowisk małymi literami w kolejności alfabetycznej.
-
+SELECT LOWER(NAME) FROM POSITIONS
+ORDER BY NAME ASC;
 
 -- 10. Znajdź wszystkich pracowników, których bonus roczny będzie większy niż dwukrotność pensji miesięcznej
-
+SELECT * FROM USERS
+WHERE SALARY_ADD * 12 > SALARY_BASE * 2;
 
 -- 11. Wyszukaj wszystkich pracowników, których id jest większe niż 150
+SELECT * FROM USERS
+WHERE ID_USER > 150;
