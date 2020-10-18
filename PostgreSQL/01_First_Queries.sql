878ad0a32558bb1e2546a9bba5f93de35d6a0b36
Adam <57396704+Sankti@users.noreply.github.com>
2020-10-18 17:23:11 +0200

    Create 01_First_Queries.sql

---
+++ PostgreSQL/01_First_Queries.sql
@@ -0,0 +1,10 @@
+/*
+* Alright so we just saw how queries are made
+* Try to complete the following query!
+*/
+
+SELECT id, firstName, lastName /* get the students id and name here */
+FROM Student;
+
+SELECT * /* get all of students info here */
+FROM Student;
